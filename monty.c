e85d4380da0a2d933fa8697a00328edfa9610896
Hiwot Ameha <hiwotameha23@gmail.com>
2022-06-16 18:50:34 +0300

    task

--- monty.c
+++ monty.c
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
 	{
 		line_number++;
 		instruction = parse_line(line);
-	
+
 		if (!(instruction->opcode) || instruction->opcode[0] == '#')
 		{
 			free(instruction);
